80aeef567c3f43f419b530ecdafdc1aa232a074f
Kaciade <valerypolyakov60@gmail.com>
2023-06-09 16:00:16 +0200

    Задание 1 выполнено

--- Task_1.java
+++ Task_1.java
@@ -2,6 +2,17 @@
 
 public class Task_1 {
     public static void main(String[] args) {
-        
+        int [] loc = {413, 812, 250, 584, 933, 767};
+        int max = loc[0];
+        int min = loc[0];
+        for(int i = 0; i != loc.length; i ++){
+            if(loc[i] > max){
+                max = loc[i];
+            }
+            if(loc[i] < min){
+                min = loc[i];
+            }
+        }
+        System.out.println("Min = " + min + "\nMax = " + max);
     }
 }
