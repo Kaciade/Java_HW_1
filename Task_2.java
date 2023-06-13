81228006b38269c691842ae78300ff8949b4eb92
Kaciade <valerypolyakov60@gmail.com>
2023-06-13 13:08:24 +0200

    Задание 2 выполнено

--- Task_2.java
+++ Task_2.java
@@ -2,6 +2,19 @@
 
 public class Task_2 {
     public static void main(String[] args) {
-        
+        fillDiagonal();
+    }
+
+    public static void fillDiagonal() {
+        int[][] arr = new int[5][5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i][i] = 1;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
